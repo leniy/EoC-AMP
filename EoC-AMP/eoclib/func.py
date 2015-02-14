fd090b5b779a3f4c07e8d853acb0301c134fbc9a
Leniy Tsan <m@leniy.org>
2015-02-14 10:21:04 +0800

    update

--- EoC-AMP/eoclib/func.py
+++ EoC-AMP/eoclib/func.py
@@ -12,8 +12,10 @@ import struct
 import time
 import wx
 import re
+import json
 from wx.lib.pubsub import setupkwargs
 from wx.lib.pubsub import pub
+from eocheadcommon import RcEocHeadCommon
 
 #==================== 定义全局变量 ====================
 
@@ -28,16 +30,10 @@ running_mode = 'console'      #运行模式，判断当前是在console中还是
 
 #==================== 创建基本功能函数（函数默认为ip可以正常访问的情况，异常状态后续处理） ====================
 
-#函数功能：ip转换数字
-#函数输入：字符串格式ip
-#函数返回：int或long格式数字
-def ip2int(ip):
+def ip2int(ip): # str格式的ip，转换为int或long格式的数字
 	return socket.htonl(struct.unpack('I', socket.inet_aton(ip))[0])
 
-#函数功能：数字转换ip
-#函数输入：int或long格式数字
-#函数返回：字符串格式ip
-def int2ip(i):
+def int2ip(i): # int或long格式的数字，转换为str格式的ip
 	return socket.inet_ntoa(struct.pack('I', socket.htonl(i)))
 
 #函数功能：从list.csv中获取全部待配置头端的IP范围
@@ -87,312 +83,19 @@ def getiplist():
 #
 
 
-#函数功能：登陆函数（默认ip可以正常访问）
-#函数输入：ip、用户名、密码
-#函数返回：cookies、错误代码（出错时，cookies是一个包含错误原因的字符串）
-def logineoc(ip, user, password):
-	#设置cookie保存登录状态
-	cookies = urllib2.HTTPCookieProcessor()
-	opener = urllib2.build_opener(cookies)
-	urllib2.socket.setdefaulttimeout(3)  #设置超时为2秒
-	#首先登陆账号，并设置cookie
-	request = urllib2.Request(
-		url	 = 'http://' + ip + '/index.html',
-		headers = {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Referer' : 'http://' + ip + '/login.html'
-		},
-		data	= 'username=' + user + '&password=' + hashlib.md5(password).hexdigest() + '&language=en&x=38&y=16',
-	)
-
-	try:
-		response = opener.open(request)
-		return cookies,0
-	except:
-		try:
-			response = opener.open(request)
-			return cookies,0
-		except:
-			print 'Error: ' + ip + ' Login Failure'
-			return 'cookies_error',-1
 
 
 
-#函数功能：获取设备中各个参数的唯一mib编号
-#函数输入：ip、cookies
-#函数返回：错误代码（编号值直接赋值给全局变量，不用返回）
-def get_dev_unique_number(ip, cookies):
-	#Eoc头端中，各个项目采用数字表示，这儿对其意义进行定义
-	#global termEocCnuIndex
-	#global termEocCnuDevName
-	#global termEocCnuDevMac
-	#global termEocCnuDevModel
-	#global termEocCnuEtherVlanPVID
-	global mib_obj_enum
-
-	opener = urllib2.build_opener(cookies)
-	request = urllib2.Request(
-		url	 = 'http://' + ip + '/js/mib_obj_enum.js',
-	)
-	try:
-		response = opener.open(request)
-		response = response.read()
-		response = response.replace('window.top.__mib_oids = ','')
-		response = response.replace(';','')
-		response = eval(response)
-		mib_obj_enum = response
-
-		#termEocCnuIndex         = str(response['termEocCnuIndex'])
-		#termEocCnuDevName       = str(response['termEocCnuDevName'])
-		#termEocCnuDevMac        = str(response['termEocCnuDevMac'])
-		#termEocCnuDevModel      = str(response['termEocCnuDevModel'])
-		#termEocCnuEtherVlanPVID = str(response['termEocCnuEtherVlanPVID'])
-		return 0
-	except:
-		print 'Error: ' + ip + ' mib_obj_enum.js can NOT be downloaded'
-		return -1
 
 
-#函数功能：获取设备系统时间（默认ip可以正常访问）
-#函数输入：ip、cookies
-#函数返回：十六进制的时间字符串、错误代码（出错时，第一个返回值是一个包含错误原因的字符串）
-def get_clock_date_and_time(ip, cookies):
-	global mib_obj_enum
-	opener = urllib2.build_opener(cookies)
-	request = urllib2.Request(
-		url	 = 'http://%s/g' % (ip),
-		headers = {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Referer' : 'http://%s//sys_timemng.html' % (ip),
-		},
-		data	= 'scalar=%s' % (mib_obj_enum['raisecomClockDateAndTime'])
-	)
-	try:
-		response = opener.open(request)
-		response = response.read()
-		response = response.replace('null','None')
-		response = response.replace('\/','/')
-		ClockDateAndTime = eval(response)
-		if ClockDateAndTime['errcode'] == 0:
-			temptime = ClockDateAndTime[str(mib_obj_enum['raisecomClockDateAndTime'])]
-			return temptime,0
-		elif ClockDateAndTime['errcode'] == -1:
-			tempstr = ClockDateAndTime['errinfo']
-			print 'errinfo: ' + tempstr
-			return tempstr,-1
-	except:
-		print 'Error: ' + ip + ' can NOT get dateandtime'
-		return 'get_dateandtime_error',-1
 
 
-#函数功能：获取Cable频段信息（默认ip可以正常访问）
-#函数输入：ip、cookies
-#函数返回：固定衰减使能、固定衰减值、错误代码（出错时，第一个返回值是一个包含错误原因的字符串）
-def get_cable_freqinfo(ip, cookies):
-	global mib_obj_enum
-	opener = urllib2.build_opener(cookies)
-	request = urllib2.Request(
-		url	 = 'http://%s/g' % (ip),
-		headers = {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Referer' : 'http://%s//eoc_cable_freqinfo.html' % (ip),
-		},
-		data	= 'entry=a:0:0;%s;%s' % (mib_obj_enum['termEocPMDCbatCablePortPowerFixAttenEnable'], mib_obj_enum['termEocPMDCbatCablePortPowerFixAtten'])
-	)
-	try:
-		response = opener.open(request)
-		response = response.read()
-		response = response.replace('null','None')
-		response = response.replace('\/','/')
-		response = eval(response)
-		if response['errcode'] == 0:
-			responsetemp = response['entry'][0]
-			return responsetemp[str(mib_obj_enum['termEocPMDCbatCablePortPowerFixAttenEnable'])],responsetemp[str(mib_obj_enum['termEocPMDCbatCablePortPowerFixAtten'])],0
-		elif response['errcode'] == -1:
-			tempstr = response['errinfo']
-			print 'errinfo: ' + tempstr
-			return tempstr,-1
-	except:
-		print 'Error: ' + ip + ' can NOT get cable_freqinfo'
-		return 'get_cable_freqinfo_error',-1
 
 
-#函数功能：获取设备列表函数（默认ip可以正常访问）
-#函数输入：ip、cookies
-#函数返回：dict格式的cnu_devlist列表、错误代码（出错时，第一个返回值是一个包含错误原因的字符串）
-def get_cnu_devlist(ip, cookies):
-	global mib_obj_enum
-
-	opener = urllib2.build_opener(cookies)
-	request = urllib2.Request(
-		url	 = 'http://%s/g' % (ip),
-		headers = {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Referer' : 'http://%s/cnu_devlist.html' % (ip),
-		},
-		data	= 'entry=a:0:0;%s;%s;%s;%s' % (mib_obj_enum['termEocCnuIndex'], mib_obj_enum['termEocCnuDevName'], mib_obj_enum['termEocCnuDevMac'], mib_obj_enum['termEocCnuDevModel'])
-	)
-	try:
-		response = opener.open(request)
-		response = response.read()
-		#print response
-		response = response.replace('null','None')
-		#首先把json的空值符号换成python的空值符号
-		response = response.replace('\/','/')
-		#然后替换转义符
-		cnu_ajax_devlist = eval(response)
-		if cnu_ajax_devlist['errcode'] == 0:
-			cnu_devlist = cnu_ajax_devlist['entry']
-			return cnu_devlist,0
-		elif cnu_ajax_devlist['errcode'] == -1:
-			tempstr = cnu_ajax_devlist['errinfo']
-			print 'errinfo: ' + tempstr
-			return tempstr,-1
-	except:
-		print 'Error: ' + ip + ' can NOT get list'
-		return 'get_cnu_devlist_error',-1
 
 
-#函数功能：获取某终端单个端口的pvid（默认ip可以正常访问）
-#函数输入：ip、cookies、port_index
-#函数返回：int格式的pvid（不必返回错误代码。pvid > 1 为正常设置，pvid = 1 为尚未设置，pvid = -9999 为系统返回错误）
-def get_port_pvid(ip, cookies, port_index):
-	global mib_obj_enum
-
-	opener = urllib2.build_opener(cookies)
-	request = urllib2.Request(
-		url	 = 'http://%s/g' % (ip),
-		headers = {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Referer' : 'http://%s/cnu_port_config.html' % (ip),
-		},
-		data	= 'entry=g:%s:0;%s' % (port_index, mib_obj_enum['termEocCnuEtherVlanPVID'])
-	)
-	#port_index设置原理：
-	#终端设备共有5个端口，分别为1个Cable、4个FE
-	#end_index定义为：某头端中，各个终端设备的索引
-	#port_index定义为：某终端设备中，各个端口的索引
-	#如果end_index为16512
-	#则这五个端口的port_index分别为16512+6000001、16512+100001、16512+100002、16512+100003、16512+100004
-	#FE的pvid，就是给设备设置的vlan。
-	try:
-		response = opener.open(request)
-		response = response.read()
-		#print response
-		response = response.replace('null','None')
-		cnu_ajax_portconfig = eval(response)
-		#print cnu_ajax_portconfig
-		if cnu_ajax_portconfig['errcode'] == 0:
-			cnu_portconfig = cnu_ajax_portconfig['entry']
-			pvid = cnu_portconfig[0][str(mib_obj_enum['termEocCnuEtherVlanPVID'])]
-			return pvid
-		elif cnu_ajax_portconfig['errcode'] == -1:
-			#tempstr = cnu_ajax_portconfig['errinfo']
-			return -9999
-	except:
-		print 'Error: ' + ip + ' get port pvid error'
-		return -9999
 
 
-#函数功能：列出某头端的全部信息（默认ip可以正常访问）
-#函数输入：字符串格式的ip
-#函数返回：单个头端Eoc中，所有Cnu的信息的列表head_info_list（单个Cnu的信息，以dict格式存储于list中）
-def show_eochead_info(ip):
-	global mib_obj_enum
-	#global termEocCnuIndex
-	#global termEocCnuDevName
-	#global termEocCnuDevMac
-	#global termEocCnuDevModel
-	#global termEocCnuEtherVlanPVID
-
-	cookies,errorcode1 = logineoc(ip,'xxxxxxxxx','xxxxxxxxxxx')  #如果登陆出错，返回错误代码，并打印错误原因
-	if errorcode1 == 0:  #如果成功登陆，再继续执行下面的代码
-		get_dev_unique_number(ip,cookies)
-		cnu_devlist,errorcode2 = get_cnu_devlist(ip,cookies)  #如果获取列表失败，返回错误代码，并打印错误原因
-		if errorcode2 == 0:  #如果列表成功获取，再继续执行下面的代码
-			#head_info_str  = ''
-			head_info_list = []
-			#head_info_str += '在Eoc头端' + ip + '中，共发现' + str(len(cnu_devlist)) + '台设备：\n'
-			#head_info_str += '"头端ip","唯一编号","终端设备号","终端MAC","终端型号","终端PVID1","终端PVID2","终端PVID3","终端PVID4"\n'
-			#根据设备列表，进行for循环
-			for cnu_dev in cnu_devlist:
-
-				Eoc_ip    = ip
-				Cnu_index = cnu_dev[str(mib_obj_enum['termEocCnuIndex'])]  #注意，这个是int格式的数字
-				Cnu_name  = 'Cnu ' + cnu_dev[str(mib_obj_enum['termEocCnuDevName'])]
-				Cnu_mac   = cnu_dev[str(mib_obj_enum['termEocCnuDevMac'])]
-				Cnu_model = cnu_dev[str(mib_obj_enum['termEocCnuDevModel'])]
-
-				port_index1 = str(100000 + Cnu_index + 1)  #获取第一个FE端口的索引号
-				port_index2 = str(100000 + Cnu_index + 2)  #获取第二个FE端口的索引号
-				port_index3 = str(100000 + Cnu_index + 3)  #获取第三个FE端口的索引号
-				port_index4 = str(100000 + Cnu_index + 4)  #获取第四个FE端口的索引号
-
-				Cnu_pvid1 = get_port_pvid(ip,cookies,port_index1)  #注意，这个是int格式的数字
-				Cnu_pvid2 = get_port_pvid(ip,cookies,port_index2)  #注意，这个是int格式的数字
-				Cnu_pvid3 = get_port_pvid(ip,cookies,port_index3)  #注意，这个是int格式的数字
-				Cnu_pvid4 = get_port_pvid(ip,cookies,port_index4)  #注意，这个是int格式的数字
-
-				#head_info_str += '"' + Eoc_ip         + '",'
-				#head_info_str += '"' + str(Cnu_index) + '",'
-				#head_info_str += '"' + Cnu_name       + '",'
-				#head_info_str += '"' + Cnu_mac        + '",'
-				#head_info_str += '"' + Cnu_model      + '",'
-				#head_info_str += '"' + str(Cnu_pvid1) + '",'
-				#head_info_str += '"' + str(Cnu_pvid2) + '",'
-				#head_info_str += '"' + str(Cnu_pvid3) + '",'
-				#head_info_str += '"' + str(Cnu_pvid4) + '"\n'
-
-				templist = [{
-					"Eoc_ip"    : Eoc_ip,
-					"Cnu_index" : Cnu_index,
-					"Cnu_name"  : Cnu_name,
-					"Cnu_mac"   : Cnu_mac,
-					"Cnu_model" : Cnu_model,
-					"Cnu_pvid1" : Cnu_pvid1,
-					"Cnu_pvid2" : Cnu_pvid2,
-					"Cnu_pvid3" : Cnu_pvid3,
-					"Cnu_pvid4" : Cnu_pvid4
-					}]
-				head_info_list.extend(templist)
-			if type(head_info_list) == list:
-				return head_info_list
-			else:
-				return []
-		else:
-			return []
-	else:
-		return []
-
-
-#函数功能：设置某终端单个端口的pvid（默认ip可以正常访问）
-#函数输入：ip、cookies、port_index（str格式）、pvid（str格式）
-#函数返回：int格式的错误代码
-def set_port_pvid(ip, cookies, port_index, pvid):
-	global mib_obj_enum
-	#global termEocCnuEtherVlanPVID
-	get_dev_unique_number(ip,cookies) #因为获取全部ip的cnu列表的时候，这些设备独立编码已经被覆盖了，所以设置pvid的时候要重新设置一次并保存回全局变量中
-	set_port_pvid_poststr = str(mib_obj_enum['termEocCnuEtherVlanPVID']) + '.' + port_index + '=' + pvid
-	#print set_port_pvid_poststr
-
-	opener = urllib2.build_opener(cookies)
-	request = urllib2.Request(
-		url	 = 'http://' + ip + '/s',
-		headers = {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Referer' : 'http://' + ip + '/cnu_port_config.html',
-		},
-		data	= set_port_pvid_poststr
-	)
-	try:
-		response = opener.open(request)
-		response = response.read()
-		#print response
-		response = eval(response)
-		return response['errcode']
-	except:
-		print 'Error: ' + ip + ' set port pvid error'
-		return -1
 
 
 #函数功能：自动检索全部头端及下属终端的信息，并配置vlan
@@ -400,28 +103,14 @@ def set_port_pvid(ip, cookies, port_index, pvid):
 #函数输出：无
 def eoc_auto_main(runtype = "console"):
 	#由于本部分功能与gui窗口属于不同的线程，为了防止冲突，配置信息直接从配置文件中获取
-#	start_ip, end_ip, start_pvid, end_pvid = getconfig()
-#	start_ip   = int(start_ip)
-#	end_ip     = int(end_ip)
-#	start_pvid = int(start_pvid)
-#	end_pvid   = int(end_pvid)
-	start_pvid = 2000
-	end_pvid   = 2999
-
 	#把所有的头端ip放到一个列表中
 	all_ip_list = []
-#	for ip_last in range(start_ip,end_ip + 1):       #城区头端
-#		#if ip_last >= 141 and ip_last <= 160 :   #特殊原因，这20个ip地址需要跳过去
-#		#	continue
-#		ip = 'x.x.x.' + str(ip_last)
-#		all_ip_list.append(ip)
 	all_ip_list.extend(getiplist())
-
 	#把所有宽带VLAN放到一个列表中（点播VLAN直接减1000计算）
+	start_pvid = 2000
+	end_pvid   = 2999
 	all_vlan_list = range(start_pvid,end_pvid + 1)
 
-	#global all_head_info_list
-	#global unset_cnu_count
 	all_head_info_list = [] #首先把全局列表清空
 	unset_cnu_count = 0 #用来定义有多少个cnu设备尚未设置pvid
 
@@ -438,7 +127,10 @@ def eoc_auto_main(runtype = "console"):
 	file_object.write(s)
 
 	for ip in all_ip_list:
-		temptemptemp = show_eochead_info(ip)
+		this_dev = RcEocHeadCommon(ip, 'x', 'x')
+		if this_dev.device_is_up == False:
+			continue
+		temptemptemp = this_dev.get_allcnu_info()
 		all_head_info_list.extend(temptemptemp)
 		if runtype == "gui":
 			#rate = 100.0 * ( ip_last - start_ip + 1 ) / ( end_ip - start_ip +1 )
@@ -503,21 +195,22 @@ def eoc_auto_main(runtype = "console"):
 	for temp_cnu in all_head_info_list: #循环检测所有的cnu
 		if temp_cnu['Cnu_pvid1'] == 1:  #如果当前cnu第一个FE网口没有设置pvid，则开始设置
 			ip = temp_cnu['Eoc_ip']
-			cookies,errorcode1 = logineoc(ip,'xxxxxxxxx','xxxxxxxxxxx')
-			if errorcode1 == 0: #重新登陆一次，并获取cookies
-				cnuindex = temp_cnu['Cnu_index'] #cnu的索引
-				port_index1 = 100000 + cnuindex + 1 #4个FE端口的索引。因为暂时没有开通其他服务，所以4个端口设置同样的pvid
-				port_index2 = 100000 + cnuindex + 2
-				port_index3 = 100000 + cnuindex + 3
-				port_index4 = 100000 + cnuindex + 4
-				temppvid = pvid_notused_list[0] #pvid设置为第一个尚未使用的值
-				pvid_notused_list = pvid_notused_list[1:] #把第一个从未使用的列表中去掉
-				set_port_pvid(ip,cookies,str(port_index1),str(temppvid))
-				print "set a pvid " + str(temppvid) + " in " + str(ip)
-				set_port_pvid(ip,cookies,str(port_index2),str(temppvid - 1000))  #1000为点播的vlan
-				set_port_pvid(ip,cookies,str(port_index3),str(1))     #1为未配置时的默认值，此时端口暂不开启
-				set_port_pvid(ip,cookies,str(port_index4),str(1))     #1为未配置时的默认值，此时端口暂不开启
-				#print ip + ", " + temp_cnu['Cnu_name'] + "   pvid: " + str(temppvid)
+			this_dev = RcEocHeadCommon(ip, 'xxxxxxxxx','xxxxxxxxxxx')
+			if this_dev.device_is_up == False:
+				continue
+			cnuindex = temp_cnu['Cnu_index'] #cnu的索引
+			port_index1 = 100000 + cnuindex + 1 #4个FE端口的索引。因为暂时没有开通其他服务，所以4个端口设置同样的pvid
+			port_index2 = 100000 + cnuindex + 2
+			port_index3 = 100000 + cnuindex + 3
+			port_index4 = 100000 + cnuindex + 4
+			temppvid = pvid_notused_list[0] #pvid设置为第一个尚未使用的值
+			pvid_notused_list = pvid_notused_list[1:] #把第一个从未使用的列表中去掉
+			this_dev.set_port_pvid(str(port_index1),str(temppvid))
+			print "set a pvid " + str(temppvid) + " in " + str(ip)
+			this_dev.set_port_pvid(str(port_index2),str(temppvid - 1000))  #1000为点播的vlan
+			this_dev.set_port_pvid(str(port_index3),str(1))     #1为未配置时的默认值，此时端口暂不开启
+			this_dev.set_port_pvid(str(port_index4),str(1))     #1为未配置时的默认值，此时端口暂不开启
+			#print ip + ", " + temp_cnu['Cnu_name'] + "   pvid: " + str(temppvid)
 	if runtype == "gui":
 		wx.MessageBox(u"搜索完成，共找到" + str(len(all_head_info_list)) + u"个设备\n\n其中，" + str(unset_cnu_count) + u"台未配置，现已配置完成",u"通知",wx.OK|wx.ICON_INFORMATION)
 #		wx.MessageBox(u"pvid设置成功",u"通知",wx.OK|wx.ICON_INFORMATION)
